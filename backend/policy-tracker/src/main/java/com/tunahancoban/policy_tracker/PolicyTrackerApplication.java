06a515bf82af80e1ff61f2a8dca9e6593d9e7497
Rache <tunahancbnn@gmail.com>
2026-06-29 10:15:04 +0300

    Project created.

---
+++ backend/policy-tracker/src/main/java/com/tunahancoban/policy_tracker/PolicyTrackerApplication.java
@@ -0,0 +1,13 @@
+package com.tunahancoban.policy_tracker;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class PolicyTrackerApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(PolicyTrackerApplication.class, args);
+	}
+
+}
